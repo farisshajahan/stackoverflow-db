b262661b85aee90d9426aeec22150f894284fcfb
Faris Shajahan <farisshajahan@protonmail.com>
2022-02-20 11:55:54 +0530

    Add linting and code formatting setup to project

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: "es5",
+  quoteProps: "consistent",
+  semi: true,
+};
